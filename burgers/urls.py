06a33129213dd61f1c1e3deb4a45a4e2edde3a6f
leyla <lvvillarroel@uc.cl>
2020-05-03 20:30:53 -0300

    Fixed trailing slashes

--- burgers/urls.py
+++ burgers/urls.py
@@ -3,12 +3,12 @@ from rest_framework_nested import routers
 from . import views
 
 
-router = routers.SimpleRouter()
-router.register(r'hamburguesa', views.BurgerView)
-router.register(r'ingrediente', views.IngredientView)
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'hamburguesa/?', views.BurgerView)
+router.register(r'ingrediente/?', views.IngredientView)
 
 burgers_router = routers.NestedSimpleRouter(
-    router, r'hamburguesa', lookup='hamburguesa')
+    router, r'hamburguesa', lookup='hamburguesa', trailing_slash=False)
 burgers_router.register(r'ingrediente', views.BurgerIngredientView)
 urlpatterns = [
     path('', include(router.urls)),
