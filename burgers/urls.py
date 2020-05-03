a565ca53012621b711b5fbc230adb9b0ea9d08ac
leyla <lvvillarroel@uc.cl>
2020-05-03 20:34:06 -0300

    Fixing trailing slashes

--- burgers/urls.py
+++ burgers/urls.py
@@ -8,8 +8,8 @@ router.register(r'hamburguesa/?', views.BurgerView)
 router.register(r'ingrediente/?', views.IngredientView)
 
 burgers_router = routers.NestedSimpleRouter(
-    router, r'hamburguesa', lookup='hamburguesa', trailing_slash=False)
-burgers_router.register(r'ingrediente', views.BurgerIngredientView)
+    router, r'hamburguesa/?', lookup='hamburguesa', trailing_slash=False)
+burgers_router.register(r'ingrediente/?', views.BurgerIngredientView)
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(burgers_router.urls)),
